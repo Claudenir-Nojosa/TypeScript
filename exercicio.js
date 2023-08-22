fe0ec1ee630544b019fc61956ce0558a96b689b7
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-08-22 20:32:45 -0300

    Exercise finished

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+//Multiplicação
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicacao(2, 4));
+//Saudação
+function dizOi(nome) {
+    return "Olá " + nome;
+}
+console.log(dizOi("Instrutor"));
---
+++ exercicio.ts
@@ -0,0 +1,14 @@
+//Multiplicação
+
+function multiplicacao(num1:number, num2:number):number {
+    return num1 * num2;
+}
+
+console.log(multiplicacao(2,4));
+
+//Saudação
+function dizOi(nome:string):string {
+    return "Olá " + nome
+}
+
+console.log(dizOi("Instrutor"));
